0c7f3f734ba47d2c1233b046554e5f5ca0d2ce60
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-10-04 14:49:20 +0200

    Try getting the string without scanf

--- ceaser-cipher.c
+++ ceaser-cipher.c
@@ -17,16 +17,15 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 void cipher(int rotNum) {
     printf("Your rot number is %d\n", rotNum);
 }
 
 int main() {
-    int rotNumber;
+    int rotNumber = 12;
     printf("Enter the rot number: ");
-    scanf("%d", &rotNumber);
+    //scanf("%d", &rotNumber);
     cipher(rotNumber);
 
     int messageSize = 10;
@@ -37,27 +36,33 @@ int main() {
         return 1;
     }
     
-    printf("Enter your message below: ");
+    printf("Enter your message below: \n");
     int c;
     int idx = 0;
 
+    printf("Just before the loop\n");
     while ((c = getchar()) != '\n' && c != EOF) {
+        printf("Looping\n");
         if (idx == messageSize -1) {
             messageSize *= 2;
             message = (char *)realloc(message, messageSize * sizeof(char));
             
             if (message == NULL) {
                 printf("realloc problem");
+                free(message);
                 return 1;
             }
         }
         message[idx++] = (char)c;
     }
-    message[idx] = '\n';
+    message[idx] = '\0';
+    printf("After the loop\n");
 
     for (int i = 0; i < idx; i++) {
         printf("%c ", message[i]);
     }
+    
+    free(message);
 
     return 0;
 
